322cdc41fd01fd14310a319519646d53c7dd26ab
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-07 14:47:07 -0400

    Updated

--- weatherapp/src/components/Weather.jsx
+++ weatherapp/src/components/Weather.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import clouds from "../assets/clouds.png";
+import "../styles/Weather.css";
 const Weather = () => {
   return (
-    <div>
+    <div className='Weather'>
+      <img src={clouds} alt="clouds" />
+      <h1 class="poppins-thin">22°C</h1>
+      <p class="poppins-thin">New York</p>
     </div>
   );
 }
