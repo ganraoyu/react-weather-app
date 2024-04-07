45b202538c7a16e8c6d5288d39a3408884f68c88
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-07 17:59:08 -0400

    Updated

--- weatherapp/src/components/WeatherDetails.jsx
+++ weatherapp/src/components/WeatherDetails.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "../styles/WeatherDetails.css"
-
+import humidity from "../assets/humidity.png"
+import wind from "../assets/wind.png"
 const WeatherDetails = () => {
   return (
-    <div>
-      
+    <div className='WeatherDetails'>
+      <div className='div1'>
+        <img src={humidity} alt="humidity" />
+        <p>64%</p>
+      </div>
+      <div className='div1'>
+        <img src={wind} alt="wind" />
+        <p>18km/h</p>
+      </div>
     </div>
   )
 }
